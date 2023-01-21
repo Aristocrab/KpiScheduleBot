68206e38ba98fcf5686b5e77b1749cef0868fe69
Maksym Kanupa <maksymkanupa@gmail.com>
2023-01-21 17:05:46 +0200

    feat: Describe api interface using Refit

---
+++ KpiSchedule.Api/IScheduleApi.cs
@@ -0,0 +1,29 @@
+﻿using KpiSchedule.Api.Entities.Exams;
+using KpiSchedule.Api.Entities.Groups;
+using KpiSchedule.Api.Entities.Lecturers;
+using KpiSchedule.Api.Entities.Lessons;
+using KpiSchedule.Api.Entities.Time;
+using Refit;
+
+namespace KpiSchedule.Api;
+
+public interface IScheduleApi
+{
+    [Get("/api/schedule/groups")]
+    Task<GroupsDto> GetGroups();
+    
+    [Get("/api/schedule/lecturer/list")]
+    Task<LecturersDto> GetLecturers();
+    
+    [Get("/api/schedule/lecturer")]
+    Task<LecturerInfo> GetLecturerById(string lecturerId);
+    
+    [Get("/api/time/current")]
+    Task<CurrentTimeDto> GetCurrentTime();
+    
+    [Get("/api/schedule/lessons")]
+    Task<LessonsDto> GetLessons(Guid groupId);
+    
+    [Get("/api/exams/group")]
+    Task<ExamsDto> GetExams(Guid groupId);
+}
